23e69f644c49242ec3574f6f73467cd652554c09
V A <V A>
2016-09-09 23:31:50 +0300

    ..

--- rimi/js/aside-filter.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  var button = document.querySelector('.aside-params-toggle-button');
-  var elem = document.querySelector('.aside-params');
-   button.addEventListener('click', function() {
-    elem.classList.toggle('aside-params--active');
-  });
-
-  });    
--- rimi/js/aside-menu.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  var button = document.querySelector('.b-aside-menu-toggle-button');
-  var elem = document.querySelector('.b-aside-menu');
-  button.addEventListener('click', function() {
-    elem.classList.toggle('b-aside-menu--active');
-  });
-
-  }); 
