2cc454ccd13433bd181ac4947d498dd88fe26a0d
V A <V A>
2016-10-19 09:37:37 +0300

    ..

--- kaspersky-page2/js/script.js
+++ kaspersky-page2/js/script.js
@@ -70,7 +70,13 @@ $('#question-ten .js-button').click(function() { setTimeout(function() {
     $.ajax({
       type: 'POST',
       url: 'result.php',
-      data: msg
+      data: msg,
+      // success: function(data) {
+      //   $('#question-form').html(data);
+      // },
+      // error:  function(xhr, str){
+      // alert('Возникла ошибка: ' + xhr.responseCode);
+      // }
     });
 }, 1000); });
 
