e5755e56796f209eb8d5b45823c5cacdf3901314
V A <V A>
2016-11-03 13:17:03 +0300

    .

--- ciklovita/js/script.js
+++ ciklovita/js/script.js
@@ -1,7 +1,7 @@
 /*menu hover*/
 
 $(document).ready(function() {
-  $('.menu__menu-flower area').hover(function() {
+  $('.menu-flower area').hover(function() {
     var parent = $(this).parent().parent();
     var num = parent.hasClass('flower_0') ? 0 : 1;
     var ind = $(this).index()+1;
