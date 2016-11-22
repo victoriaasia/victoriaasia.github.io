a962936fcc6aea97920e0a72f24d8e2847428fd0
V A <V A>
2016-11-22 15:35:44 +0300

    mobile fixed

--- contacts/js/script.js
+++
@@ -1,7 +0,0 @@
- var contactsApp = angular.module('contactsApp', []);
-
- contactsApp.controller('contactsCtrl', function ($scope, $http){
-   $http.get('js/data.json').success(function(data) {
-     $scope.contacts = data;
-   });
-  });
