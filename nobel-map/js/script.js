7b678fe858bc90c982b2bfcb659fa68b5a4f8687
V A <V A>
2016-11-27 21:32:50 +0300

    .

--- nobel-map/js/script.js
+++ nobel-map/js/script.js
@@ -2,6 +2,6 @@ var word = document.querySelectorAll('.nob-word');
 
 for (let i=0; i<word.length; i++) {
   word[i].addEventListener('click', function() {
-    word[i].classList.toggle('hover-effect');
+    word[i].classList.toggle('hover-effect');      
   });
 }
