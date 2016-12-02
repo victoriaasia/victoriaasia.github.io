1b1690e805baf9fad2c9485f87d090da722c009e
V A <V A>
2016-12-02 21:49:01 +0300

    .

--- ded-moroz/js/script.js
+++ ded-moroz/js/script.js
@@ -2,10 +2,12 @@ window.onload = function() {
 
   var video = document.getElementById("dm-video");
 
+  video.addEventListener("contextmenu", function (e) { e.preventDefault(); e.stopPropagation(); }, false);
+
   var playButton = document.getElementById("dm-js-control");
 
   playButton.addEventListener("click", function() {
-    if (video.paused == true) {
+    if (video.paused) {
       video.play();
       playButton.classList.toggle("dm-control-hide");
     } else {
