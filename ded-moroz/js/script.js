0e9f196270720ab46a27765968f043ff2abdbc13
V A <V A>
2016-12-02 21:56:48 +0300

    .

--- ded-moroz/js/script.js
+++ ded-moroz/js/script.js
@@ -1,10 +1,13 @@
 window.onload = function() {
 
   var video = document.getElementById("dm-video");
+  var playButton = document.getElementById("dm-js-control");
 
   video.addEventListener("contextmenu", function (e) { e.preventDefault(); e.stopPropagation(); }, false);
 
-  var playButton = document.getElementById("dm-js-control");
+  if (video.hasAttribute("controls")) {
+    video.removeAttribute("controls")
+  } 
 
   playButton.addEventListener("click", function() {
     if (video.paused) {
