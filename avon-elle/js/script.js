ca3a51b121018eaf93b56a90ce38ca9702874f1d
V A <V A>
2016-12-04 17:08:50 +0300

    fixed

--- avon-elle/js/script.js
+++
@@ -1,7 +0,0 @@
-var word = document.querySelectorAll('.nob-word');
-
-for (let i=0; i<word.length; i++) {
-  word[i].addEventListener('click', function() {
-    word[i].classList.toggle('hover-effect');      
-  });
-}
