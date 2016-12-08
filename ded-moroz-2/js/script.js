f363cad2f5c96bec6718972fc1be88de34e9572c
V A <V A>
2016-12-08 14:26:22 +0300

    .

---
+++ ded-moroz-2/js/script.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+
+  var video = document.getElementById('dm-video');
+  var playButton = document.getElementById('dm-js-control');
+
+  video.addEventListener('contextmenu', function (e) { e.preventDefault(); e.stopPropagation(); }, false);
+
+  if (video.hasAttribute('controls')) {
+    video.removeAttribute('controls')
+  }
+
+  playButton.addEventListener('click', function() {
+    if (video.paused) {
+      video.play();
+      playButton.classList.toggle('dm-control-hide');
+    } else {
+      video.pause();
+      playButton.classList.toggle('dm-control-hide');
+    }
+  });
+};
+
+
+var btn = document.querySelector('.dm-js-btn');
+var wrapperBlock = document.querySelector('.-first');
+var block = document.querySelector('.dm-js-content');
+var blockResult = document.querySelector('.dm-result-content');
+
+btn.onclick = function(event) {
+  event = event || window.event;
+  if (event.preventDefault) {
+    event.preventDefault();
+  } else {
+    event.returnValue = false;
+  }
+}
+
+btn.addEventListener("click", function(){
+    setTimeout(function(){
+      block.classList.add('dm-hidden');
+      wrapperBlock.classList.add('-first-result');
+      blockResult.classList.remove('dm-hidden');
+    }, 1500);
+});
