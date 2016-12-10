b28f91ef129b979bf99160bea751d1a0aa241587
V A <V A>
2016-12-10 18:29:19 +0300

    .

--- ded-moroz/js/script.js
+++ ded-moroz/js/script.js
@@ -59,10 +59,10 @@ $(document).ready(function() {
     $('#dm-overlay').fadeIn(200,
       function(){
       if(window.matchMedia('(min-width: 769px)').matches) {
-      $('#dm-modal-2').css('display', 'block').animate({opacity: 1, top: '40px'}, 300);
+      $('#dm-modal-3').css('display', 'block').animate({opacity: 1, top: '40px'}, 300);
       }	else if(window.matchMedia('(max-width: 768px)').matches) {
-      $('#dm-modal-2').css('display', 'block').animate({opacity: 1, top: '0px'}, 300);
-      $('html').css('overflow', 'hidden');
+      $('#dm-modal-3').css('display', 'block').animate({opacity: 1, top: '0px'}, 300);
+      $('body').css('overflow', 'hidden');
       $('.js-modal').css('overflow', 'auto');
       }
      });
