ae3ab8b1da7bca718469a2f168b9994771919376
V A <V A>
2017-03-22 22:24:03 +0300

    .

--- market/js/script.js
+++ market/js/script.js
@@ -18,6 +18,8 @@ $(document).ready(function(){
     dots: true
   });
 
+  // cut text - index page
+
   $('.js-desc').each (function(){
    if ($(this).text().length > 90)
      $(this).text($(this).text().substring(0, 90) + '...');
@@ -28,6 +30,8 @@ $(document).ready(function(){
      $(this).text($(this).text().substring(0, 1800) + '...');
   });
 
+  // tab content - products page
+
   $('.js-items li').click(function(){
 		var tab_id = $(this).attr('data-tab');
 
@@ -37,5 +41,30 @@ $(document).ready(function(){
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
 	});
+
+  // modal - rent page
+	$('.js-title, .js-document').click( function(event){
+		event.preventDefault();
+		$('.overlay').fadeIn(200,
+			function(){
+			$('.js-modal').css('display', 'block').animate({opacity: 1}, 300);
+	 });
+	});
+	$('.js-close, .overlay').click( function(){
+		$('.js-modal').animate({opacity: 0}, 200,
+				function(){
+					$(this).css('display', 'none');
+					$('.overlay').fadeOut(400);
+				}
+			);
+	});
+
+
+  $('#top').on('click', function (e) {
+      e.preventDefault();
+      $('html,body').animate({
+          scrollTop: 0
+      }, 700);
+  });
 
 });
