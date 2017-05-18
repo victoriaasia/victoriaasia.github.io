c64991961eb96128cededae9904c3942f1cb497a
victoriaasia <yes.im@bk.ru>
2017-05-18 22:10:29 +0300

    .

--- quora/js/script.js
+++ quora/js/script.js
@@ -147,3 +147,36 @@ $('.js-main-menu li').click(function(){
  $(this).addClass('current');
  $("#"+tab_id).addClass('current');
 });
+
+
+// hide header onscroll
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 2;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    if (st > lastScrollTop && st > navbarHeight){
+        $('header').removeClass('nav-down').addClass('header-up');
+    } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('header-up').addClass('header-down');
+        }
+    }
+    lastScrollTop = st;
+}
