23a20585e0e80d6fd445da4f1a9abe77267a1f6b
victoriaasia <yes.im@bk.ru>
2017-11-14 14:32:54 +0300

    .

--- skillscv/js/main.js
+++ skillscv/js/main.js
@@ -46,6 +46,21 @@ var $html = $(document.documentElement);
 var $body = $(document.body);
 
 $document.ready(function() {
+
+	$('.datepicker').on('click', function(e) {
+		e.preventDefault();
+	}).datepicker({
+		dateFormat: "yy-mm-dd",
+		prevText: '',
+		nextText: '',
+		monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+		monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+		dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+		changeMonth: true,
+		changeYear: true,
+		isRTL: false
+	});
+
 	$('#jsslider').roundSlider({
 		sliderType: 'min-range',
 		editableTooltip: false,
